5aa0d9ec6ba8739af093b80e86010e65127513db
Michael Gurnett <mgurnett@gmail.com>
2025-05-08 16:17:48 -0600

    added users

--- Dashboard/settings.py
+++ Dashboard/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
     # libraries
     'bootstrap5',
+    'django_extensions',
     # apps
     'api',
     'core',
--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
+from datetime import datetime, date, timedelta
+from icecream import ic
+from django.contrib.auth.models import User
+
 
 # Create your models here.
