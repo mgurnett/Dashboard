ecf7e0a86601a94c40c84f80a8e89edaacc45973
Michael Gurnett <mgurnett@gmail.com>
2025-05-09 17:32:45 -0600

    moved the filesout

--- materials-django-user-management-/user_auth_intro/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-from . import views
-
-urlpatterns = [
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("dashboard/", views.dashboard, name="dashboard"),
-    path("sign_up/", views.sign_up, name="sign_up"),
-]
